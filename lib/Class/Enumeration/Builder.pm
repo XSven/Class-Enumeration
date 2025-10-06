e6576c59213604127973690ebc6b5e77ba577ce0
Sven Willenbuecher <sven.willenbuecher@gmx.de>
2025-10-06 14:44:22 +0200

    Rename some variables

--- lib/Class/Enumeration/Builder.pm
+++ lib/Class/Enumeration/Builder.pm
@@ -8,8 +8,10 @@ use parent ();
 sub import {
   shift;
 
-  my $enum_class = caller;
-  parent->import( -inheritor => $enum_class, 'Class::Enumeration' );
+  # $class == enum class
+  my $class = caller;
+
+  parent->import( -inheritor => $class, 'Class::Enumeration' );
 
   my @values;
   my $ordinal = 0;
@@ -18,15 +20,15 @@ sub import {
   if ( ref $_[ 1 ] eq 'HASH' ) {
     # TODO: getters for custom attributes have to be setup in enum class
     while ( my ( $name, $attributes ) = splice @_, 0, 2 ) {
-      push @values, $enum_class->new( $ordinal++, $name, $attributes )
+      push @values, $class->new( $ordinal++, $name, $attributes )
     }
   } else {
-    @values = map { $enum_class->new( $ordinal++, $_ ) } @_
+    @values = map { $class->new( $ordinal++, $_ ) } @_
   }
 
   {
     no strict 'refs'; ## no critic ( ProhibitNoStrict )
-    *{ "$enum_class\::Values" } = \@values;
+    *{ "$class\::Values" } = \@values;
   }
 }
 
